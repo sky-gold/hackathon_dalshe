1d8529ff40a61cf3dd61b8de17a327b0c0cddf11
Nikita Efimov <nikita-efimov04@mail.ru>
2024-05-19 09:32:44 +0300

    add faq_qa in consts

--- tg_bot/faq.py
+++ tg_bot/faq.py
@@ -1,23 +1,16 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from consts import FAQ_QA
 
-faq_qa = [
-    (("Узнать свой риск рака груди", "Как узнать вероятность рака груди у меня"), "В этом калькуляторе вы можете узнать свой риск рака груди: https://www.dalshefond.ru/check/"),
-    (("Подозрение на рак", "Обратится к онкологу"), "Персональное обращение к онкологу: https://vmesteplus.ru/personal/personalized-help/oncologist/"),
-    (("Пособие по профилактике", "Как сохранить здоровье груди"), "Пособие по профилактике, https://dalshefond.ru/prevention-manual/"),
-    (("Как рак груди лечится", "Лечение рака молочный железы"), "Онлайн курс 'Что важно знать о раке молочной железы': https://vmesteplus.ru/distance-programs/oncologist-course/"),
-    (("Получить консультацию медицинского юриста"), "Консультация медицинского юриста: https://vmesteplus.ru/personal/personalized-help/lawyer/"),
-    (("Групповая психотерапия"), "Консультация медицинского юриста: https://vmesteplus.ru/personal/personalized-help/lawyer/"),
-]
 def only_words_str(s):
     return s.replace("?", "").replace(",", "").lower()
 
 def get_most_similar_faq(new_question):
     faq_questions = []
     faq_id = []
-    for i in range(len(faq_qa)):
-        for j in range(len(faq_qa[i][0])):
-            faq_questions.append(only_words_str(faq_qa[i][0][j]))
+    for i in range(len(FAQ_QA)):
+        for j in range(len(FAQ_QA[i][0])):
+            faq_questions.append(only_words_str(FAQ_QA[i][0][j]))
             faq_id.append((i, j))
     vectorizer = TfidfVectorizer()
     question_vectors = vectorizer.fit_transform(faq_questions)
@@ -33,4 +26,4 @@ def get_most_similar_faq(new_question):
     while i2 == i1:
         x += 1
         i2, j2 = sort_id[x][1], sort_id[x][2]
-    return (faq_qa[i1][0][j1], faq_qa[i1][1]), (faq_qa[i2][0][j2], faq_qa[i2][1])
+    return (FAQ_QA[i1][0][j1], FAQ_QA[i1][1]), (FAQ_QA[i2][0][j2], FAQ_QA[i2][1])
